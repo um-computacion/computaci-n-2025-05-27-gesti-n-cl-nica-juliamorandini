88b3f85c7d4b22b51770d2013c404f523f639862
juliamorandini <j.morandini@alumno.um.edu.ar>
2025-06-07 12:06:12 -0300

    agregado de clase turno y receta

---
+++ clase_receta.py
@@ -0,0 +1,19 @@
+from clase_paciente import Paciente
+from clase_medico import Medico
+from datetime import datetime
+class Receta :
+    def __init__ (self, paciente: Paciente, medico: Medico, medicamentos: list[str], fecha: datetime)
+        self.paciente = paciente
+        self.medico = medico
+        self.medicamentos = medicamentos
+        self.fecha = fecha
+    
+    def __str__(self):
+        medicamentos_str = "[" + ", ".join(self.__medicamentos) + "]"
+        paciente_str = str(self.__paciente)
+        medico_str = str(self.__medico).replace("\n", "\n    ")
+        return (f"Receta(\n"
+                f"  Paciente({paciente_str}),\n"
+                f"  Medico(\n    {medico_str}\n  ),\n"
+                f"  {medicamentos_str},\n"
+                f"  {self.__fecha.strftime('%Y-%m-%d %H:%M:%S')}\n")
---
+++ clase_turno.py
@@ -0,0 +1,22 @@
+from clase_paciente import Paciente
+from clase_medico import Medico
+import datetime
+class Turno:
+    def __init__(self, paciente: Paciente, medico: Medico, fecha_hora: datetime, especialidad: str):
+        self.paciente = paciente
+        self.medico = medico
+        self.fecha_hora = fecha_hora
+        self.especialidad = especialidad
+
+    def obtener_medico(self) -> Medico:
+        return self.__medico
+
+    def obtener_fecha_hora(self) -> datetime:
+        return self.__fecha_hora
+    
+def __str__(self) -> str:
+        return (f"Turno:\n"
+                f"  Paciente: {self.__paciente}\n"
+                f"  Médico: {self.__medico}\n"
+                f"  Especialidad: {self.__especialidad}\n"
+                f"  Fecha y hora: {self.__fecha_hora.strftime('%d/%m/%Y %H:%M')}")
